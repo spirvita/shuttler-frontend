90e688ae3e72b5dc95d6026616ee25386621abd3
Vic <syuan0212@gmail.com>
2025-04-17 17:16:28 +0800

    chore: add Prettier

--- nuxt-app/eslint.config.mjs
+++ nuxt-app/eslint.config.mjs
@@ -1,6 +1,26 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import prettierPlugin from "eslint-plugin-prettier";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true, // 使用分號
+          singleQuote: false, // 使用單引號
+          tabWidth: 2, // 縮排為 2 個空格
+          vueIndentScriptAndStyle: true, // 縮排 <script> 和 <style>
+          singleAttributePerLine: true, // 每個屬性換行
+          htmlWhitespaceSensitivity: "ignore", // 忽略 HTML 空白敏感度
+        },
+      ],
+      "no-console": "warn",
+      "vue/multi-word-component-names": "off",
+    },
+  },
+]);
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
-const countStore = useCountStore();
+  const countStore = useCountStore();
 </script>
 
 <template>
   <div class="text-center">
     <h1 class="text-2xl mb-5">Index Page</h1>
     <p>Count: {{ countStore.count }}</p>
-    <ElButton type="primary" class="text-black" @click="countStore.addCount" >Add Count</ElButton>
-    <ElButton type="success" @click="countStore.subtractCount">Subtract Count</ElButton>
-    <ElButton type="danger" @click="countStore.resetCount">Reset Count</ElButton>
+    <ElButton
+      type="primary"
+      class="text-black"
+      @click="countStore.addCount"
+    >
+      Add Count
+    </ElButton>
+    <ElButton
+      type="success"
+      @click="countStore.subtractCount"
+    >
+      Subtract Count
+    </ElButton>
+    <ElButton
+      type="danger"
+      @click="countStore.resetCount"
+    >
+      Reset Count
+    </ElButton>
   </div>
 </template>
 
