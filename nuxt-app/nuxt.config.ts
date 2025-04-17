72e8432f0b34d1c8a666439781c981a7fabf9d5e
Vic <syuan0212@gmail.com>
2025-04-17 17:06:39 +0800

    chore: add Element-Plus, sass, and dayjs dependencies

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -4,12 +4,22 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@pinia/nuxt"],
+  modules: ["@nuxt/eslint", "@pinia/nuxt", "@element-plus/nuxt"],
   typescript: {
     typeCheck: true,
   },
   css: ["~/assets/css/main.css"],
+  elementPlus: {
+    importStyle: "scss",
+  },
   vite: {
     plugins: [tailwindcss()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/css/styles/element/index.scss";`,
+        },
+      },
+    },
   },
 });
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -6,9 +6,9 @@ const countStore = useCountStore();
   <div class="text-center">
     <h1 class="text-2xl mb-5">Index Page</h1>
     <p>Count: {{ countStore.count }}</p>
-    <button @click="countStore.addCount">Add Count</button>
-    <button @click="countStore.subtractCount">Subtract Count</button>
-    <button @click="countStore.resetCount">Reset Count</button>
+    <ElButton type="primary" class="text-black" @click="countStore.addCount" >Add Count</ElButton>
+    <ElButton type="success" @click="countStore.subtractCount">Subtract Count</ElButton>
+    <ElButton type="danger" @click="countStore.resetCount">Reset Count</ElButton>
   </div>
 </template>
 
