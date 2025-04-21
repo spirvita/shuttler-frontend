b4e8ec6a27fcf5d4cdb84f69fa5259fc57488fd4
Vic <syuan0212@gmail.com>
2025-04-21 14:07:18 +0800

    feat: add dynamic routing for activities

---
+++ nuxt-app/pages/activities/[activity].vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+  const router = useRoute();
+</script>
+<template>
+  <div>
+    <h2>{{ router.fullPath }}</h2>
+  </div>
+</template>
---
+++ nuxt-app/pages/activities/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <div class="text-center">Page Activities</div>
+  </div>
+</template>
+<style scoped></style>
