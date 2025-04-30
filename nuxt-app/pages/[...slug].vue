248aa66be7074e8243090ba7b4e43d4e10e625e4
Vic <syuan0212@gmail.com>
2025-04-30 15:00:17 +0800

    feat: add 404 page

---
+++ nuxt-app/pages/[...slug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const router = useRouter();
+  const event = useRequestEvent();
+  if (event) setResponseStatus(event, 404);
+  router.push("/");
+</script>
+<template>
+  <div>
+    <div class="text-center">沒有這個頁面喔</div>
+  </div>
+</template>
+<style scoped></style>
