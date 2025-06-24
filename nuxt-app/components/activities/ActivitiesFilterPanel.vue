42c5e1cdf37746245f96087586b51385911a75c0
Vic <syuan0212@gmail.com>
2025-06-24 16:48:29 +0800

    fix: activities filter

--- nuxt-app/components/activities/ActivitiesFilterPanel.vue
+++ nuxt-app/components/activities/ActivitiesFilterPanel.vue
@@ -8,7 +8,7 @@
     Calendar,
     Clock,
     Money,
-    CloseBold
+    RefreshRight
   } from "@element-plus/icons-vue";
   import { useTwLocationState } from "@/composables/useTwLocationState";
   import { useShuttlerLevelOptions } from "@/composables/useShuttlerLevelOptions";
@@ -17,6 +17,7 @@
 
   const route = useRoute();
   const city = (route.query.city as string) || "";
+  const startTimeSlot = ref("00:00");
   const emits = defineEmits(["updateFilter"]);
   const {
     twCitiesOptions,
@@ -34,7 +35,7 @@
     level: (route.query.level as string) || "",
     date: "",
     timeSlot: "",
-    points: 0,
+    points: 1000,
     city: ""
   });
   const customDatePrefix = shallowRef({
@@ -42,6 +43,13 @@
       return h("sapn", "");
     }
   });
+  const marks = {
+    0: "0",
+    100: "100",
+    300: "300",
+    500: "500",
+    1000: "1000"
+  };
   const isOutsideTwoWeeksOrPastDate = (date: string): boolean => {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
@@ -63,7 +71,7 @@
       level: "",
       date: "",
       timeSlot: "",
-      points: 0,
+      points: 1000,
       city: ""
     };
     manuallySetCity("");
@@ -78,8 +86,11 @@
     (newFilter) => {
       if (newFilter.date === null) activitiesFilter.value.timeSlot = "";
       const filter = JSON.parse(JSON.stringify(activitiesFilter.value));
-      if (filter.level !== "")
+      if (filter.level) {
         filter.level = mapLevelToLevel(filter.level).toString();
+      } else {
+        filter.level = "";
+      }
       emits("updateFilter", filter);
     },
     { deep: true, immediate: true }
@@ -90,6 +101,28 @@
     activitiesFilter.value.zipCode = newValue[1];
     emits("updateFilter", activitiesFilter.value);
   });
+
+  // 日期選擇為當天，則時段選擇開始時間為現在時間 +1 時段
+  watch(
+    () => activitiesFilter.value.date,
+    (newDate) => {
+      if (newDate) {
+        const today = new Date();
+        const selectedDate = new Date(newDate);
+        if (
+          selectedDate.getFullYear() === today.getFullYear() &&
+          selectedDate.getMonth() === today.getMonth() &&
+          selectedDate.getDate() === today.getDate()
+        ) {
+          startTimeSlot.value = `${today.getHours() + 1}:00`;
+        } else {
+          startTimeSlot.value = "00:00";
+        }
+      } else {
+        startTimeSlot.value = "00:00";
+      }
+    }
+  );
 </script>
 
 <template>
@@ -107,10 +140,10 @@
         class="p-3"
         size="large"
         :text="true"
-        :icon="CloseBold"
+        :icon="RefreshRight"
         @click="resetActivitiesFilter"
       >
-        清除篩選
+        恢復預設
       </el-button>
     </div>
     <div class="flex items-center pb-4 border-b border-neutral-300">
@@ -133,7 +166,7 @@
       <el-icon class="mr-1.5"><Place /></el-icon>
       <el-select
         v-model="twDistrict"
-        placeholder="請選擇區域(需有縣市)"
+        placeholder="選擇區域(需有縣市)"
         :value-on-clear="''"
         clearable
         :disabled="!twCity"
@@ -150,7 +183,7 @@
       <el-icon class="mr-1.5"><UserFilled /></el-icon>
       <el-select
         v-model="activitiesFilter.spotsLeft"
-        placeholder="請選擇人數"
+        placeholder="請選擇需報名人數"
         clearable
       >
         <el-option
@@ -165,7 +198,7 @@
       <el-icon class="mr-1.5"><Odometer /></el-icon>
       <el-select
         v-model="activitiesFilter.level"
-        placeholder="請選擇程度"
+        placeholder="請選擇可參與程度"
         clearable
       >
         <el-option
@@ -182,7 +215,7 @@
         v-model="activitiesFilter.date"
         type="date"
         class="w-full"
-        placeholder="請選擇日期"
+        placeholder="請選擇可參與日期"
         clearable
         value-format="YYYY-MM-DD"
         :disabled-date="(date: string) => isOutsideTwoWeeksOrPastDate(date)"
@@ -193,11 +226,10 @@
       <el-icon class="mr-1.5"><Clock /></el-icon>
       <el-time-select
         v-model="activitiesFilter.timeSlot"
-        start="00:00"
+        :start="startTimeSlot"
         step="01:00"
         end="23:00"
-        :disabled="!activitiesFilter.date"
-        placeholder="請先選擇時段(需有日期)"
+        placeholder="請選擇可參與時段"
         prefix-icon=""
         clearable
       />
@@ -211,11 +243,9 @@
           :step="100"
           show-stops
           :max="1000"
+          :marks="marks"
         />
       </div>
-      <span class="text-neutral-500 text-sm">
-        最低篩選 {{ activitiesFilter.points }} 點以上
-      </span>
     </div>
   </div>
 </template>
